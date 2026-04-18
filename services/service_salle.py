b4bc0091fe49a691e37c3e0e8bdad0094b248dbf
stone42-upload <arielstone883@gmail.com>
2026-04-18 19:11:11 -0400

    Implementation de la méthode supprimer_salle(code) afin de supprimer une salle identifier par son code. bien que peut d'instruction, j'ai suivi le même chemein logique donc la suppression si le code est présent, et si il correspond à une salle dans la bd

--- services/service_salle.py
+++ services/service_salle.py
@@ -20,6 +20,7 @@ class ServiceSalle:
             print("Cette salle a été correctement ajouter")
             return True
 
+
     def modifier_salle(self, salle):
         for donnee in (salle.code, salle.description, salle.categorie):
             if not donnee:
@@ -37,3 +38,16 @@ class ServiceSalle:
             self.dao_salle.update_salle(salle)
             print("Cette salle a été correctement modifiée")
             return True
+
+    def supprimer_salle(self, code):
+        if not code:
+            print("Erreur : le code de la salle est obligatoire pour la supprimer")
+            return False
+        salle = self.dao_salle.get_salle(code)
+        if not salle:
+            print("Erreur : la salle n'existe pas")
+            return False
+
+        self.dao_salle.delete_salle(code)
+        print("La salle a été correctement supprimée")
+        return True
