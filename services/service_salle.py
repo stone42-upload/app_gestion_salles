7bffe5fa070f37c188ad6104282fb3fde3956658
stone42-upload <arielstone883@gmail.com>
2026-04-18 18:55:43 -0400

    Modification de la méthode(correction d'une erreur) ajouter_salle(salle) pour insert_salle(salle) et implémentation de la méthode update_salle(salle) permettant de mettre à jour les informations d'une salle après un ensemble de vérification

--- services/service_salle.py
+++ services/service_salle.py
@@ -16,6 +16,24 @@ class ServiceSalle:
         if salle.capacite < 1:
             print("Désolé une salle dont la capacité est inférieur à 1 ne peut pas être ajouter")
         else:
-            self.dao_salle.ajouter_salle(salle)
+            self.dao_salle.insert_salle(salle)
             print("Cette salle a été correctement ajouter")
+            return True
+
+    def modifier_salle(self, salle):
+        for donnee in (salle.code, salle.description, salle.categorie):
+            if not donnee:
+                print("Erreur: une donnée obligatoire est manquante." )
+                return False
+
+        if salle.capacite is None:
+            print("Erreur : capacité manquante.")
+            return False
+
+        if salle.capacite < 1:
+            print("capacité doit être supérieure ou égale à 1 pour modifier cette salle avec ces valeurs")
+            return False
+        else:
+            self.dao_salle.update_salle(salle)
+            print("Cette salle a été correctement modifiée")
             return True
