360961611b951803ce579271d9cdb2b7825103e5
stone42-upload <arielstone883@gmail.com>
2026-04-18 18:40:17 -0400

    Implémentation de la méthode ajouter_salle(salle) qui permet d'ajouter une salle lorsqu'il respecte une un ensemble de condition

--- services/service_salle.py
+++ services/service_salle.py
@@ -3,3 +3,19 @@ class ServiceSalle:
     def __init__(self, dao_salle):
         self.dao_salle = dao_salle
 
+    def ajouter_salle(self, salle):
+        for donnee in (salle.code, salle.description, salle.categorie):
+            if not donnee:
+                print("Erreur: une donnée obligatoire est manquante." )
+                return False
+
+        if salle.capacite is None:
+            print("Erreur : capacité manquante.")
+            return False
+
+        if salle.capacite < 1:
+            print("Désolé une salle dont la capacité est inférieur à 1 ne peut pas être ajouter")
+        else:
+            self.dao_salle.ajouter_salle(salle)
+            print("Cette salle a été correctement ajouter")
+            return True
