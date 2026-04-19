f159ce2d45daf533584257e6e5f972d7cd1a32ac
stone42-upload <arielstone883@gmail.com>
2026-04-18 21:28:11 -0400

    implémentation du cadre Information Salle j'ai utiliser ici une boucle car c'étais très répétitif de le faire un à un. Je n'ai utiliser aucune ia et pour justification, sticky=w permet de coller un widget à gauche de sa cellule dans la grid et setattr permet de creer automatiquement un objet(dynamiquement)

--- views/view_salle.py
+++ views/view_salle.py
@@ -6,4 +6,23 @@ class ViewSalle(ctk.CTk):
 
         self.service_salle = service_salle
 
+        self.title("Gestion des Salles")
+        self.geometry("600x400")
 
+        self.frame_info = ctk.CTkFrame(self)
+        self.frame_info.pack(pady=20 ,padx="top", fill="x")
+
+        champs = [
+            ("Code :", "entry_code"),
+            ("Description :", "entry_description"),
+            ("Catégorie :", "entry_categorie"),
+            ("Capacité :", "entry_capacite")
+        ]
+
+        for i, (label_text, entry_attr) in enumerate(champs):
+            lbl = ctk.CTkLabel(self.frame_info, text=label_text)
+            lbl.grid(row=i, column=0, padx=10, pady=5, sticky="w")
+
+            entry = ctk.CTkEntry(self.frame_info)
+            entry.grid(row=i, column=1, padx=10, pady=5)
+            setattr(self, entry_attr, entry)
